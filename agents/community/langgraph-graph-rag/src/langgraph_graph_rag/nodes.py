e8bedcbbb17ce601beaca7b66ef836469d348be3
Mateusz Switala <mateusz.switala@ibm.com>
2025-08-20 12:38:57 +0200

    improve graph
    Signed-off-by: Mateusz Switala <mateusz.switala@ibm.com>

--- agents/community/langgraph-graph-rag/src/langgraph_graph_rag/nodes.py
+++ agents/community/langgraph-graph-rag/src/langgraph_graph_rag/nodes.py
@@ -1,4 +1,4 @@
-from typing import Annotated, Sequence, Any
+from typing import Annotated, Sequence, Any, List
 
 from typing_extensions import TypedDict
 
@@ -7,6 +7,7 @@ from langgraph.graph.message import add_messages
 from langchain_ibm import ChatWatsonx, WatsonxEmbeddings
 
 from langchain_neo4j import GraphCypherQAChain, Neo4jGraph, Neo4jVector
+from langchain_neo4j.vectorstores.neo4j_vector import remove_lucene_chars
 
 from langchain_core.messages import BaseMessage
 from langchain_core.prompts import ChatPromptTemplate
@@ -17,18 +18,20 @@ from pydantic import BaseModel, Field
 
 class AgentState(TypedDict):
     # The add_messages function defines how an update should be processed
-    # Default is to replace. add_messages says "append"
     question: str
-    documents: list[str]
+    structured_data: str
+    unstructured_data: List[str]
     messages: Annotated[Sequence[BaseMessage], add_messages]
 
 
+# Extract entities from text
 class Entities(BaseModel):
-    """List of identified entities."""
+    """Identifying information about entities."""
 
-    names: list[str] = Field(
+    names: List[str] = Field(
         ...,
-        description="Distinct, individual elements (entities), including people, organizations, or companies that appear in the text.",
+        description="All the person, organization, or business entities that "
+        "appear in the text.",
     )
 
 
@@ -38,7 +41,9 @@ class GraphNodes:
         self.llm = ChatWatsonx(
             model_id=kwargs.get("model_id"), watsonx_client=api_client
         )
-        self.graph = Neo4jGraph(url="bolt://localhost:7687", username="", password="")
+        self.graph = Neo4jGraph(
+            url="bolt://localhost:7687", username="neo4j", password="password"
+        )
 
         embedding_func = WatsonxEmbeddings(
             model_id=kwargs.get("embedding_model_id"), watsonx_client=api_client
@@ -48,10 +53,10 @@ class GraphNodes:
             graph=self.graph,
             embedding=embedding_func,
             index_name="vector",
+            keyword_index_name="keyword",
             search_type="hybrid",
             node_label="Document",
             embedding_node_property="embedding",
-            text_node_properties=["text"],
         )
 
     def _get_graph_qa_chain(self) -> GraphCypherQAChain:
@@ -87,43 +92,76 @@ class GraphNodes:
         entity_chain = chat_prompt | self.llm.with_structured_output(Entities)
 
         return entity_chain.invoke({"question": question}).names
-    
-    def graph_search(self, state: AgentState) -> AgentState:
-        question = state["question"]
 
+    def _generate_full_text_query(self, input_text: str) -> str:
+        """
+        Generate a full-text search query for a given input string.
+
+        This function constructs a query string suitable for a full-text search.
+        It processes the input string by splitting it into words and appending a
+        similarity threshold (~2 changed characters) to each word, then combines
+        them using the AND operator. Useful for mapping entities from user questions
+        to database values, and allows for some misspelings.
+        """
+        full_text_query = ""
+        words = remove_lucene_chars(input_text).split()
+        for word in words[:-1]:
+            full_text_query += f" {word}~2 AND"
+        full_text_query += f" {words[-1]}~2"
+        return full_text_query.strip()
+
+    def graph_search(self, question: str) -> AgentState:
         entities = self._retrieve_entities(question)
 
         for entity in entities:
-            self.graph.query(
-            """CALL db.index.fulltext.queryNodes('entity', $query, {limit:2})
-            YIELD node,score
-            CALL {
-              WITH node
-              MATCH (node)-[r:!MENTIONS]->(neighbor)
-              RETURN node.id + ' - ' + type(r) + ' -> ' + neighbor.id AS output
-              UNION ALL
-              WITH node
-              MATCH (node)<-[r:!MENTIONS]-(neighbor)
-              RETURN neighbor.id + ' - ' + type(r) + ' -> ' +  node.id AS output
-            }
-            RETURN output LIMIT 50
-            """,
-            {"query": generate_full_text_query(entity)},
-        )
+            response = self.graph.query(
+                """CALL db.index.fulltext.queryNodes('entity', $query, {limit:2})
+                YIELD node,score
+                CALL {
+                MATCH (node)-[r:!MENTIONS]->(neighbor)
+                RETURN node.id + ' - ' + type(r) + ' -> ' + neighbor.id AS output
+                UNION
+                MATCH (node)<-[r:!MENTIONS]-(neighbor)
+                RETURN neighbor.id + ' - ' + type(r) + ' -> ' +  node.id AS output
+                }
+                RETURN output LIMIT 50
+                """,
+                {"query": self._generate_full_text_query(entity)},
+            )
+            result += "n".join([str(el) for el in response])
+        return result
 
-    def graph_qa(self, state: AgentState) -> AgentState:
-        """Returns a dictionary of at least one of the GraphState"""
-        """ Invoke a Graph QA Chain """
-
+    def retriever(self, state: AgentState) -> AgentState:
         question = state["messages"][-1].content
-        state["question"] = question
+        structured_data = self.graph_search(question)
+        unstructured_data = [
+            el.page_content for el in self.vector_index.similarity_search(question)
+        ]
 
-        graph_qa_chain = self._get_graph_qa_chain(state)
+        return {
+            "structured_data": structured_data,
+            "unstructured_data": unstructured_data,
+            "question": question,
+        }
 
-        result = graph_qa_chain.invoke(
-            {
-                # "context": graph.schema,
-                "query": question,
-            },
+    def generate(self, state: AgentState) -> AgentState:
+        final_data = f"""Structured data:
+    {state["structured_data"]}
+    Unstructured data:
+    {"#Document ".join(state["unstructured_data"])}
+        """
+        user_prompt = (
+            f"""Answer the question based only on the following context:
+{final_data}
+
+Question: {state["question"]}
+""",
         )
-        return {"documents": result, "question": question}
+        system_prompt = "You are helpful assistant"
+        response = self.llm.invoke(
+            [
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ]
+        )
+        return {"messages": [response]}
