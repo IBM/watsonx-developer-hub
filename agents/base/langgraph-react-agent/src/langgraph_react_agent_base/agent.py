d02258e5d5ccbdbdcaf6c317352f460596b70cd9
Mateusz Szewczyk <139469471+MateuszOssGit@users.noreply.github.com>
2026-01-23 10:33:55 +0100

    chore: Update base/langgraph template with GenAI runtime (#89)
    * chore: Update base/langgraph template with GenAI runtime
    
    * chore: update dependecnies

--- agents/base/langgraph-react-agent/src/langgraph_react_agent_base/agent.py
+++ agents/base/langgraph-react-agent/src/langgraph_react_agent_base/agent.py
@@ -2,8 +2,8 @@ from typing import Callable
 
 from ibm_watsonx_ai import APIClient
 from langchain_ibm import ChatWatsonx
-from langgraph.graph.graph import CompiledGraph
 from langgraph.prebuilt import create_react_agent
+from langgraph.graph.state import CompiledStateGraph
 
 from langgraph_react_agent_base import TOOLS
 
@@ -12,17 +12,17 @@ def get_graph_closure(client: APIClient, model_id: str) -> Callable:
     """Graph generator closure."""
 
     # Initialise ChatWatsonx
-    chat = ChatWatsonx(model_id=model_id, watsonx_client=client, params={"temperature": 0.01})
+    chat = ChatWatsonx(
+        model_id=model_id, watsonx_client=client, params={"temperature": 0.01}
+    )
 
     # Define system prompt
     default_system_prompt = "You are a helpful AI assistant, please respond to the user's query to the best of your ability!"
 
-    def get_graph(system_prompt=default_system_prompt) -> CompiledGraph:
+    def get_graph(system_prompt=default_system_prompt) -> CompiledStateGraph:
         """Get compiled graph with overwritten system prompt, if provided"""
-        
+
         # Create instance of compiled graph
-        return create_react_agent(
-            chat, tools=TOOLS, state_modifier=system_prompt
-        )
+        return create_react_agent(chat, tools=TOOLS, prompt=system_prompt)
 
     return get_graph
