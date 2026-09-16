bc2605719d81d6c78bd437f6c6384db42fae457c
Rafal Chrzanowski <Rafal.Chrzanowski@ibm.com>
2026-09-16 08:48:45 +0200

    Fixed issues with AI service chat application
    Signed-off-by: Rafal Chrzanowski <Rafal.Chrzanowski@ibm.com>

--- apps/base/nextjs-chat-with-ai-service/next.config.ts
+++ apps/base/nextjs-chat-with-ai-service/next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next";
-import { version } from "./package.json"
+import { version } from "./package.json";
+import path from "path";
+
+const plexFontPath = path.resolve(__dirname, "node_modules/@ibm/plex");
 
 const nextConfig: NextConfig = {
   env: {
-    version
+    version,
+  },
+  sassOptions: {
+    loadPaths: [path.resolve(__dirname, "node_modules")],
+    additionalData: `@use "@carbon/styles/scss/config" with ($font-path: "${plexFontPath}");`,
   },
 };
 
